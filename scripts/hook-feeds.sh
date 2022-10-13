64e1b39f347594203e8165dcf1aab68d2e572a7a
DanielEcho <jck2@foxmail.com>
2022-10-13 12:19:50 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -11,14 +11,14 @@
 pushd customfeeds
 
 # Add luci-app-eqos
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
 
 # Add luci-proto-modemmanager
 svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
 
 # Add luci-app-gowebdav
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
+# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+# svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
 
 # Add tmate
 git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
@@ -27,7 +27,7 @@ git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
 
 # Add minieap
-svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
+# svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
 
 # Replace smartdns with the official version
 #rm -rf packages/net/smartdns
